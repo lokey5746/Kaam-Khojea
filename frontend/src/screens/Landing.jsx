fe275f495b8ed90d0adb515b02eecdd1a2222768
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-08-17 23:10:51 +0545

    landing page complete

--- frontend/src/screens/Landing.jsx
+++ frontend/src/screens/Landing.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="Kaam-Khojea" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.
+          </p>
+          <Link to="/signup" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/signin" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="Kaam Khojea" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
