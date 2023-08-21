72728af2e1cd29466a38623ba261adf7e999435d
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-08-21 22:16:17 +0545

    Frontend - register page added

--- frontend/src/screens/SignUp.jsx
+++ frontend/src/screens/SignUp.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import { FormRow, Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>
-      <h1>SignUp Page</h1>
-      <Link to="/signin">SignIn Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="john" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="smith"
+        />
+        <FormRow type="text" name="location" defaultValue="earth" />
+        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="Demon@1867!" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/signin" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
